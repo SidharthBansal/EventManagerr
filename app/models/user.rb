8b3086f3bac51e8e7e8f02fdd02832dabe12848d
Nicolas Santiago Blair <nsantiagoblair@gmail.com>
2017-12-18 19:21:17 +0100

    User/Event associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+ has_many :invitations, class_name: "Event",
+                        foreign_key: "host_id",
+                        dependent: :destroy
+
+ has_many :invites,  class_name: "Event",
+                     foreign_key: "guest_id",
+                     dependent: :destroy
+
+ has_many :guests, through: :invitations, source: :guest
+ has_many :hosts, through: :invites, source: :host
+
+
 end
