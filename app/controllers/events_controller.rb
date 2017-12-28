383b3fe3ff29c3e2bb7fddcf4097cd746e75dffd
Nick <nsantiagoblair@gmail.com>
2017-12-28 12:38:03 +0100

    add events index pagination

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,7 @@ class EventsController < ApplicationController
   before_action :logged_in_user, only: [:show, :new, :create, :edit, :destroy]
 
   def index
-    @events = Event.all
+    @events = Event.paginate(page: params[:page], per_page: 12)
   end
 
   def show
