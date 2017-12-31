04b647ea56a9e477e9d31c6893b5f87936810fb9
Nick <nsantiagoblair@gmail.com>
2017-12-31 18:46:20 +0000

    fix comments form

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,6 @@ class Comment < ApplicationRecord
     belongs_to :event
     
     validates :body, presence: true
+    validates :user_id, presence: true
+    validates :event_id, presence: true
 end
