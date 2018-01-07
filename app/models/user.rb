fc8593c2f780f1495b00bb96381ba08b5ff97638
Nick <nsantiagoblair@gmail.com>
2018-01-07 18:12:23 +0000

    edit devise in User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:facebook]
+  devise :omniauthable, :omniauth_providers => [:facebook]
 
   has_many :hosted_events,  class_name: "Event",
                             foreign_key: "host_id",
